4707258d2a0172994305ebdd309f21bbb0565a96
notsgnik <labruillere@gmail.com>
2014-07-14 16:08:05 +0200

    wireframe

--- media/js/notsfx.maintest.js
+++ media/js/notsfx.maintest.js
@@ -7,6 +7,8 @@
       																		  |
 ------------------------------------------------------------------------------*/
 
+
+
 documentReady(function(){
 	var someTimeStamp = new Date().getTime();
 	var someFPS = 0;
@@ -30,7 +32,10 @@ documentReady(function(){
 			[1,-1,1],
 			[-1,-1,1]
 		];
+		this.faces = [[0,1,2,3],[1,5,6,2],[5,4,7,6],[4,0,3,7],[0,4,5,1],[3,2,6,7]];
 		this.angles = [0,0,0];
+		this.drawStyle = "rectangles";
+
 		this.mouseIsDown = false;
 		this.mousePos = false;
 		this.prevMousePos = false;
@@ -51,7 +56,8 @@ documentReady(function(){
 	nfx.beforeStart = function(){
 		this.c = document.getElementById("myCanvas");
 		this.ctx = this.c.getContext("2d");
-		this.ctx.fillStyle = "#FFFFFF";
+		this.ctx.fillStyle = "#000000";
+		this.ctx.strokeStyle = '#FFFFFF';
 		this.ctx.font = "10px Arial";
 		this.fpsPos = [this.resolution[0] - 50,this.resolution[1] - 20];
 		this.unpause();
@@ -65,17 +71,62 @@ documentReady(function(){
 			someCount = 0;
 		}
 		someCount = someCount + 1 ;
-
-		// Display 
-
+		
+		// draw style
+		if(this.isPressed("W")){
+			this.drawStyle = "points";
+		}
+		else if(this.isPressed("X")){
+			this.drawStyle = "lines";
+		}
+		else{
+			this.drawStyle = "rectangles";
+		}
+		// Display cleaning 
+		this.ctx.fillStyle = "#000000";
 		this.ctx.clearRect(0, 0, this.resolution[0], this.resolution[1]);
-		ctx.fillText(someFPS + " fps",this.fpsPos[0],this.fpsPos[1]);
-		var r,p,x,y;
+
+		this.ctx.fillStyle = "#FFFFFF";
+		this.ctx.fillText(someFPS + " fps",this.fpsPos[0],this.fpsPos[1]);
+
+		var r,p,x,y,t = [];
 		for(var v in this.vertices){
 			r = this.p3D.rotXYZ(this.vertices[v],this.angles);
 			p = this.p3D.project2D(r,this.resolution[0], this.resolution[1], 256, 4);
-			this.ctx.fillRect(parseInt(p[0]),parseInt(p[1]),1,1);
+			t.push(p);
 		}
+		if(this.drawStyle == "points"){
+			for(var p in t){
+				this.ctx.fillRect(parseInt(t[p][0]),parseInt(t[p][1]),1,1);
+			}
+		}
+		else if(this.drawStyle == "lines"){
+			this.ctx.beginPath();
+			for(var f in this.faces){
+				this.ctx.moveTo(parseInt(t[this.faces[f][0]][0]), parseInt(t[this.faces[f][0]][1]));
+				this.ctx.lineTo(parseInt(t[this.faces[f][1]][0]), parseInt(t[this.faces[f][1]][1]));
+				this.ctx.lineTo(parseInt(t[this.faces[f][2]][0]), parseInt(t[this.faces[f][2]][1]));
+				this.ctx.lineTo(parseInt(t[this.faces[f][3]][0]), parseInt(t[this.faces[f][3]][1]));
+			}
+			this.ctx.closePath();
+			this.ctx.stroke();
+		}
+		else{
+			this.ctx.beginPath();
+			for(var f in this.faces){
+				this.ctx.moveTo(parseInt(t[this.faces[f][0]][0]), parseInt(t[this.faces[f][0]][1]));
+				this.ctx.lineTo(parseInt(t[this.faces[f][1]][0]), parseInt(t[this.faces[f][1]][1]));
+				this.ctx.lineTo(parseInt(t[this.faces[f][2]][0]), parseInt(t[this.faces[f][2]][1]));
+				this.ctx.lineTo(parseInt(t[this.faces[f][3]][0]), parseInt(t[this.faces[f][3]][1]));
+			}
+			this.ctx.closePath();
+			this.ctx.stroke();
+		}
+		//ctx.restore();
+		//this.ctx.fillStyle = "#000000";
+		//this.ctx.fillRect(0,0, this.resolution[0], this.resolution[1]);
+		//this.ctx.fillStyle = "#FFFFFF";
+		
 
 		// Pressed keys or mouse mouvement
 		if(this.mouseIsDown == true 
@@ -83,7 +134,10 @@ documentReady(function(){
 		&& this.prevMousePos != false){
 			var x = this.prevMousePos[0] - this.mousePos[0];
 			var y = this.prevMousePos[1] - this.mousePos[1];
-			this.angles = [(this.angles[0] - x) % 360, (this.angles[1] - y) % 360 , this.angles[2]];
+			if(x != 0 && y != 0){
+				this.angles = [(this.angles[0] - x) % 360, (this.angles[1] - y) % 360 , this.angles[2]];
+			}
+			
 		}
 		else{
 			var pressedKeyMove = false;
